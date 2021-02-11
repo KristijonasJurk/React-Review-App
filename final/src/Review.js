b2febf1f4a2b048aeed8b5eb626304013838f619
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-11 15:59:58 +0200

    found gramatical error... those are the worst

--- final/src/Review.js
+++ final/src/Review.js
@@ -4,6 +4,7 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+  
   const checkNumber = (number) => {
     if (number > people.length - 1) {
       return 0;
